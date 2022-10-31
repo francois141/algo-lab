d192f0642589afef14319f8890fe4edb19ce54dc
francois141 <47925812+francois141@users.noreply.github.com>
2022-10-31 14:35:13 +0100

    Create Octopussy.cpp

---
+++ problems-of-the-week/Octopussy.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+  
+  int n;
+  cin >> n;
+  
+  vector<int> t(n);
+  for(int i = 0; i < n;i++)
+    cin >> t[i];
+  
+  for(int i = 1; 2*i + 2 < n;i++) {
+    int parent = (i - 1) / 2;
+    t[i] = min(t[i],t[parent]-1);
+  }
+  
+  sort(t.begin(),t.end());
+  
+  for(int i = 0; i < n;i++)  {
+    if(t[i] <= i) {
+      cout << "no" << "\n";
+      return;
+    }
+  }
+  
+  cout << "yes" << "\n";
+  return;
+}
+
+signed main() {
+
+  int t;
+  cin >> t;
+  
+  while(t--)
+    solve();
+  
+  return 0;
+}
