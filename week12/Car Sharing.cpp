93c2d23953c05a0fed6e1085e1de284c72c908c4
francois141 <47925812+francois141@users.noreply.github.com>
2023-01-21 22:28:15 +0100

    Update Car Sharing.cpp

--- week12/Car Sharing.cpp
+++ week12/Car Sharing.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/cycle_canceling.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
 #include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 #include <boost/graph/find_flow_cost.hpp>
+
+#define int long
 
 using namespace std;
 
@@ -47,13 +50,26 @@ struct request {
   int profit;
 };
 
+vector<map<int,int>> compressed;
+vector<int> prefix;
+
+void insert(int station, int time) {
+  if(compressed[station].find(time) == compressed[station].end()) {
+    compressed[station][time] = compressed[station].size();
+  }
+}
+
+int getIndex(int station, int time) {
+  return prefix[station] + compressed[station][time];
+}
+
 void solve()
 {
   int N,S;
   cin >> N >> S;
   
-  const int max_price = 100;
-  const int max_cars = 10000;
+  const int max_profit  = 100;
+  const int max_time = INT_MAX / 8;
   
   vector<int> cars = vector<int>(S);
   for(int i = 0; i < S;i++) {
@@ -73,55 +89,55 @@ void solve()
     requests[i].to--;
   }
   
-  set<int> s = set<int>();
-  for(int i = 0; i < N;i++) {
-    s.insert(requests[i].departure);
-    s.insert(requests[i].arrival);
-  }
-  
-  int size = s.size();
-  
-  map<int,int> translate;
-  int counter = 0;
-  for(auto e : s) {
-    translate[e] = counter++;
-  }
-  
-  graph G(size*S);
-  edge_adder adder(G);
-    
-  auto v_source = boost::add_vertex(G);
-  auto v_sink = boost::add_vertex(G);
-  
-  for(int i = 0; i < size-1; i++) {
-    for(int j = 0; j < S;j++) {
-      adder.add_edge(i*S + j,(i+1)*S + j,max_cars,max_price);
-    } 
-  }
-  
-  for(int j = 0; j < S;j++) {
-    adder.add_edge(v_source,j,cars[j],max_price);
-    adder.add_edge((size-1)*S + j,v_sink,max_cars,max_price);
-  }
+  compressed = vector<map<int,int>>(S);
   
   for(int i = 0; i < N;i++) {
     auto req = requests[i];
-    int from = translate[req.departure];
-    int to = translate[req.arrival];
-    int j1 = req.from;
-    int j2 = req.to;
-    int price = (to-from)*max_price - req.profit;
-    adder.add_edge(from*S + j1,to*S + j2,1,price);
+    insert(req.from,req.departure);
+    insert(req.to,req.arrival);
   }
   
+  prefix = vector<int>(S);
+  for(int i = 1; i < S;i++) {
+    prefix[i] = prefix[i-1] + compressed[i-1].size();
+  }
+  
+  graph G(prefix[S-1] + compressed[S-1].size());
+  edge_adder adder(G);
+  
+  auto v_source = boost::add_vertex(G);
+  auto v_sink = boost::add_vertex(G);
+  
+  // Setup graph for stations
+  for(int i = 0; i < S; i++) {
+    if(compressed[i].size() == 0) continue;
+    auto it = compressed[i].begin();
+    int prev_time = it->first;
+    adder.add_edge(v_source,getIndex(i,prev_time),cars[i], (prev_time) * max_profit);
+    
+    it++;
+    for(; it != compressed[i].end(); ++it) {
+      int curr_time = it->first;
+      adder.add_edge(getIndex(i,prev_time), getIndex(i, curr_time), INT_MAX, (curr_time - prev_time) * max_profit);
+      prev_time = curr_time;
+    }
+    
+    adder.add_edge(getIndex(i,prev_time),v_sink, INT_MAX, (max_time - prev_time) * max_profit);
+  }
+  
+  // Setup requests right now 
+  for(const auto &req: requests) {
+    adder.add_edge(getIndex(req.from,req.departure),getIndex(req.to,req.arrival),1,(req.arrival - req.departure) * max_profit - req.profit);
+  }
+  
+  int flow = boost::push_relabel_max_flow(G,v_source,v_sink);
   boost::successive_shortest_path_nonnegative_weights(G, v_source, v_sink);
-  long flow = boost::find_flow_cost(G);
-  flow = max_price * nb_cars * (size+1) - flow;
-  cout << flow << endl;
+  int cost = boost::find_flow_cost(G);
+
+  cout << flow * max_time * max_profit - cost << "\n";
 }
 
-int main()
-{
+signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0); 
   
