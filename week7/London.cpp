f88487ac797b21ce63647cfbaf3654ccc906f70e
francois141 <47925812+francois141@users.noreply.github.com>
2023-01-21 11:03:14 +0100

    Update London.cpp

--- week7/London.cpp
+++ week7/London.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
+
+#define int long
 
 using namespace std;
 
@@ -34,77 +33,66 @@ class edge_adder {
       r_map[e] = rev_e;
       r_map[rev_e] = e;
     }
-
 }; 
 
 void solve() {
-  
   int h,w;
   cin >> h >> w;
   
-  string n;
-  cin >> n;
+  string s;
+  cin >> s;
   
-  vector<string> front(h);
-  vector<string> back(h);
+  vector<int> count(26,0);
+  for(int i = 0; i < s.size();i++) {
+    count[s[i] - 'A']++; 
+  }
   
-  for(int i = 0; i < h;i++)
-    cin >> front[i];
-    
-  for(int i = 0; i < h;i++)
-    cin >> back[i];
+  vector<string> sfront(h);
+  vector<string> sback(h);
   
-  vector<int> letters(26,0);
-  for(char c : n) 
-    letters[c - 'A']++;
+  for(int i = 0; i < h;i++) {
+    cin >> sfront[i];
+  }
+  for(int i = 0; i < h;i++) {
+    cin >> sback[i];
+  }
+  
+  vector<vector<int>> matrix(26,vector<int>(26,0));
+  
+  for(int i = 0; i < h;i++) {
+    for(int j = 0; j < w;j++) {
+      matrix[sfront[i][j] - 'A'][sback[i][w - j - 1] - 'A']++;
+    }
+  }
   
   graph G(26*26 + 26);
   edge_adder adder(G);
   
-  vertex_desc v_source = boost::add_vertex(G);
-  vertex_desc v_sink = boost::add_vertex(G);
-
-  vector<vector<int>> letterMatrix(26,vector<int>(26,0));
-  
-  for(int i = 0; i < h;i++) {
-    for(int j = 0; j < w;j++) {
-      const int idx1 = front[i][j] - 'A';
-      const int idx2 = back[i][w-j-1] - 'A'; 
-      letterMatrix[idx1][idx2]++;
-    }
-  }
-    
-  const int offset = 26*26;
+  auto v_source = boost::add_vertex(G);
+  auto v_sink = boost::add_vertex(G);
   
   for(int i = 0; i < 26;i++) {
     for(int j = 0; j < 26;j++) {
-      if(letterMatrix[i][j] != 0) {
-        int idx = i*26 + j;
-        adder.add_edge(v_source,idx,letterMatrix[i][j]);
-        adder.add_edge(idx,offset+i,letterMatrix[i][j]);
-        adder.add_edge(idx,offset+j,letterMatrix[i][j]);
-      }
+      adder.add_edge(v_source,26*i + j, matrix[i][j]);
+      adder.add_edge(26*i + j, 26*26 + i,INT_MAX); 
+      adder.add_edge(26*i + j, 26*26 + j,INT_MAX); 
     }
   }
 
-  for(int i = 0; i < 26;i++) {
-    adder.add_edge(offset + i, v_sink, letters[i]);
+  for(int k = 0; k < 26; k++) {
+    adder.add_edge(26*26 + k, v_sink, count[k]);
   }
   
-  int flow = boost::push_relabel_max_flow(G, v_source, v_sink);
+  long flow = boost::push_relabel_max_flow(G,v_source,v_sink);
   
-  if(flow == n.size()) {
+  if(flow == s.size())  {
     cout << "Yes" << "\n";
-  }
-  else {
+  } else {
     cout << "No" << "\n";
   }
-
-  return;
 }
 
 signed main() {
-  
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   
