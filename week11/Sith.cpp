2a3616709971cd44017a56958e054ff0e0ec96b5
francois141 <47925812+francois141@users.noreply.github.com>
2023-01-22 16:05:14 +0100

    Update Sith.cpp

--- week11/Sith.cpp
+++ week11/Sith.cpp
@@ -7,80 +7,72 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/connected_components.hpp>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K; 
+#define int long
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Triangulation_vertex_base_with_info_2<int,K> Vb;
 typedef CGAL::Triangulation_face_base_2<K> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
 typedef CGAL::Delaunay_triangulation_2<K,Tds> Triangulation;
 
-typedef CGAL::Point_2<K> Point;
+typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::undirectedS> graph;
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> graph;
-typedef boost::graph_traits<graph>::vertex_descriptor vertex_desc;    
-typedef boost::graph_traits<graph>::edge_iterator edge_it; 
+typedef CGAL::Point_2<K> Point;
 
 using namespace std;
 
-#define int long
+int n,r;
+vector<pair<Point,int>> planets;
+
+int testSize(int middle) {
+  Triangulation t;
+  t.insert(planets.begin() + middle, planets.end());
+  
+  graph G(n - middle);
+  
+  for(auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
+    if(t.segment(e).squared_length() <= r*r) {
+      auto idx1 = e->first->vertex((e->second + 1) % 3)->info() - middle;
+      auto idx2 = e->first->vertex((e->second + 2) % 3)->info() - middle;
+      boost::add_edge(idx1,idx2,G);
+    }
+  }
+  
+  vector<int> component(n-middle);
+  boost::connected_components(G,&component[0]);
+
+  vector<int> size(n-middle,0);
+  for(auto c : component) size[c]++;
+
+  return *max_element(size.begin(),size.end());
+}
 
 void solve() {
-  
-  int n,r;
   cin >> n >> r;
   
-  vector<pair<Point,int>> positions(n);
+  planets = vector<pair<Point,int>>(n);
   for(int i = 0; i < n;i++) {
     int x,y;
     cin >> x >> y;
-    positions[i] = make_pair(Point(x,y),i);
+    planets[i] = make_pair(Point(x,y),i);
   }
-
-  int start = 1;
-  int end = n;
+  
+  int start = 0;
+  int end = planets.size();
   
   while(start != end) {
-  
     int middle = (start + end + 1) / 2;
-    
-    Triangulation t;
-    t.insert(positions.begin() + middle, positions.end());
-    
-    int currentSize = n - middle;
-    
-    graph G(currentSize);
-    
-    for(auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
-      int u = e->first->vertex((e->second+1)%3)->info();
-      int v = e->first->vertex((e->second+2)%3)->info();
-      if(t.segment(e).squared_length() <= r*r) {
-        boost::add_edge(u - middle,v - middle,G);
-      }
-    }
-    
-    vector<int> component_map(currentSize);
-    int ncc = boost::connected_components(G, &component_map[0]);
-    
-    int biggest = 0;
-    vector<int> compSize(currentSize,0);
-    for(int i = 0; i < currentSize;i++) {
-      int c = component_map[i];
-      compSize[c]++;
-      biggest = max(biggest,compSize[c]);
-    } 
-    
-    if(middle <= biggest) {
+    if(testSize(middle) >= middle) {
       start = middle;
-    }
-    else {
+    } else {
       end = middle-1;
     }
   }
   
-  cout << start << endl;
+  cout << start << "\n";
 }
 
-signed main() {
-  
+signed main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   
@@ -89,6 +81,4 @@ signed main() {
   
   while(t--)
     solve();
-  
-  return 0;
 }
