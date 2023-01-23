bac1ca567abbc77b3944c4868418c6db43fe8cee
francois141 <47925812+francois141@users.noreply.github.com>
2023-01-23 12:05:57 +0100

    Update Worldcup.cpp

--- week11/Worldcup.cpp
+++ week11/Worldcup.cpp
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpz.h>
 
-#define int long
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
-typedef CGAL::Point_2<K> Point;
+#define int long
 
 typedef int IT;
 typedef CGAL::Gmpz ET;
@@ -19,9 +15,13 @@ typedef CGAL::Gmpz ET;
 typedef CGAL::Quadratic_program<IT> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
+typedef CGAL::Point_2<K> Point;
+
 using namespace std;
 
-long floor_to_long(const CGAL::Quotient<CGAL::Gmpz>& x)
+int floor_to_long(const CGAL::Quotient<CGAL::Gmpz>& x)
 {
   double a = std::floor(CGAL::to_double(x));
   while (a > x) a -= 1;
@@ -30,42 +30,40 @@ long floor_to_long(const CGAL::Quotient<CGAL::Gmpz>& x)
 }
 
 struct Warehouse {
-  Point pos;
+  Point p;
   int supply;
   int alcohol;
 };
 
 struct Stadium {
-  Point pos;
+  Point p;
   int demand;
-  int maxAlcohol;
+  int upper;
 };
 
 void solve() {
-  
   int n,m,c;
   cin >> n >> m >> c;
   
-  vector<Point> points = vector<Point>(n+m); 
-  vector<Warehouse> warehouses = vector<Warehouse>(n);
-  vector<Stadium> stadiums = vector<Stadium>(m);
-  vector<vector<int>> revenues = vector<vector<int>>(n,vector<int>(m));
-  vector<pair<Point, int>> contours(0);
+  vector<Point> points(0);
   
+  vector<Warehouse> warehouses(n);
   for(int i = 0; i < n;i++) {
     int x,y,s,a;
     cin >> x >> y >> s >> a;
-    warehouses[i] = {Point(x,y), s, a};
-    points[i] = Point(x,y);
+    warehouses[i] = {Point(x,y),s,a};
+    points.push_back(Point(x,y));
   }
   
-  for(int i = 0; i < m;i++) {
+  vector<Stadium> stadiums(m);
+  for(int j = 0; j < m;j++) {
     int x,y,d,u;
     cin >> x >> y >> d >> u;
-    stadiums[i] = {Point(x,y), d, 100*u};
-    points[n+i] = Point(x,y);
+    stadiums[j] = {Point(x,y),d,100*u}; 
+    points.push_back(Point(x,y));
   }
   
+  vector<vector<int>> revenues(n,vector<int>(m));
   for(int i = 0; i < n;i++) {
     for(int j = 0; j < m;j++) {
       cin >> revenues[i][j];
@@ -75,71 +73,67 @@ void solve() {
   Triangulation t;
   t.insert(points.begin(),points.end());
   
-  for(int i = 0; i < c;i++) {
+  vector<pair<Point,int>> contours(0);
+  for(int k = 0; k < c;k++) {
     int x,y,r;
     cin >> x >> y >> r;
     Point p = Point(x,y);
-    int distance = CGAL::squared_distance(p,t.nearest_vertex(p)->point());
-    if(distance < r*r)
-      contours.push_back({p,r*r});
+    Point p2 = t.nearest_vertex(p)->point();
+    if(CGAL::squared_distance(p,p2) < r*r) {
+      contours.push_back(make_pair(Point(x,y),r*r));
+    }
   }
   
-  Program lp(CGAL::SMALLER, true, 0, false, 0); 
-  int counter = 0;
+  Program lp (CGAL::SMALLER, true, 0, false, 0);
+  int equation = 0;
   
-  // Set constraints for supply
+  // bind supply
   for(int i = 0; i < n;i++) {
-    for(int j = 0; j < m;j++)
-      lp.set_a(i*m + j,counter,1);
-    lp.set_b(counter,warehouses[i].supply);
-    counter++;
+    for(int j = 0; j < m;j++) lp.set_a(i*m + j,equation,1);
+    lp.set_b(equation,warehouses[i].supply);
+    equation++;
   }
-   
-  // Set constraints for alcohol & quantity
+  
+  // bind quantity and alcohol
   for(int j = 0; j < m;j++) {
-    for(int i = 0; i < n;i++) {
-      lp.set_a(j + i*m,counter,warehouses[i].alcohol);
-    }
-    lp.set_b(counter,stadiums[j].maxAlcohol);
-    counter++;
     
-    for(int i = 0; i < n;i++) {
-      lp.set_a(j + i*m,counter,1);
-    }
-    lp.set_b(counter,stadiums[j].demand);
-    counter++;
+    for(int i = 0; i < n;i++) lp.set_a(i*m + j, equation, 1);
+    lp.set_b(equation, stadiums[j].demand);
+    equation++;
     
-    for(int i = 0; i < n;i++) {
-      lp.set_a(j + i*m,counter,-1);
-    }
-    lp.set_b(counter,-stadiums[j].demand);
-    counter++;
+    for(int i = 0; i < n;i++) lp.set_a(i*m + j, equation, -1);
+    lp.set_b(equation, -stadiums[j].demand);
+    equation++;
+    
+    for(int i = 0; i < n;i++) lp.set_a(i*m + j, equation, warehouses[i].alcohol);
+    lp.set_b(equation, stadiums[j].upper);
+    equation++;
   }
-
-  // Set objective function
+  
   for(int i = 0; i < n;i++) {
     for(int j = 0; j < m;j++) {
-      int revenue = 100*revenues[i][j];
+      int rev = 100*revenues[i][j];
       for(auto c: contours) {
-        int distance1 = CGAL::squared_distance(warehouses[i].pos, c.first);
-        int distance2 = CGAL::squared_distance(stadiums[j].pos, c.first);
-        if(distance1 < c.second && distance2 > c.second) revenue -= 1;
-        if(distance2 < c.second && distance1 > c.second) revenue -= 1;
+        auto dist1 = CGAL::squared_distance(warehouses[i].p,c.first);
+        auto dist2 = CGAL::squared_distance(stadiums[j].p, c.first);
+        if(dist1 < c.second && dist2 > c.second) rev--;
+        if(dist1 > c.second && dist2 < c.second) rev--;
       }
-      lp.set_c(i*m+j, -revenue);
+      lp.set_c(i*m+j, -rev);
     }
   }
   
-  // Get max revenue
-  Solution s = CGAL::solve_linear_program(lp,ET());
-  if(s.is_infeasible()) 
+  Solution s = CGAL::solve_linear_program(lp, ET());
+  
+  if(s.is_infeasible()) {
     cout << "RIOT!" << "\n";
-  else 
-    cout << floor_to_long(-s.objective_value() / 100) << endl;
+    return;
+  }
+  
+  cout << floor_to_long(-s.objective_value() / 100) << "\n";
 }
 
 signed main() {
-  
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   
@@ -148,6 +142,4 @@ signed main() {
   
   while(t--)
     solve();
-  
-  return 0;
 }
