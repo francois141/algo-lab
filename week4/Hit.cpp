08274362ee397558683f69582c61485eb1517b6e
francois141 <47925812+francois141@users.noreply.github.com>
2023-01-28 15:21:01 +0100

    Update Hit.cpp

--- week4/Hit.cpp
+++ week4/Hit.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Point_2.h>
-#include <CGAL/Ray_2.h>
-#include <CGAL/Segment_2.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Ray_2<K> Ray;
-typedef CGAL::Point_2<K> Point;
-typedef CGAL::Segment_2<K> Segment;
+#define int long
 
 using namespace std;
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+typedef CGAL::Point_2<K> Point;
+typedef CGAL::Segment_2<K> Segment;
+typedef CGAL::Ray_2<K> Ray;
+
 void solve(int n) {
+  int x,y,a,b;
+  cin >> x >> y >> a >> b;
   
-  long int a,b,x,y;
-  cin >> a >> b >> x >> y;
-  Ray ray = Ray(Point(a,b),Point(x,y));
+  Ray ray = Ray(Point(x,y),Point(a,b));
   
-  vector<Segment> segments(0);
+  vector<Segment> segments(n);
   for(int i = 0; i < n;i++) {
-    long int a,b,c,d;
-    cin >> a >> b >> c >> d;
-    segments.push_back(Segment(Point(a,b),Point(c,d)));
+    int r,s,t,u;
+    cin >> r >> s >> t >> u;
+    segments[i] = Segment(Point(r,s),Point(t,u));
   }
   
-  for(Segment seg : segments) {
-    if(CGAL::do_intersect(ray,seg)) {
-      cout << "yes\n";
-      return;
+  random_shuffle(segments.begin(),segments.end());
+  
+  bool hit = false;
+  
+  for(auto seg: segments) {
+    if(CGAL::do_intersect(seg,ray)) {
+      hit = true;
+      break;
     }
   }
   
-  cout << "no\n";
+  cout << ((hit) ? "yes" : "no") << endl;
 }
 
-int main() {
-  
+signed main() {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(0);
   
   int n;
   cin >> n;
@@ -49,4 +50,5 @@ int main() {
     cin >> n;
   }
   
+  return 0;
 }
